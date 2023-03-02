3c563e45d541bf67bbd36b48e8b1e1614de0e6e5
NameawaShinderu <73576564+NameawaShinderu@users.noreply.github.com>
2023-03-02 17:08:06 +0530

    Update mycode.py

--- conversiomns/mycode.py
+++ conversiomns/mycode.py
@@ -5,15 +5,19 @@ import matplotlib.pyplot as plt
 import soundfile as sf
 
 text = input("enter the text:")
+text = text.lower().replace(" ", "")
+
 #slices = list(text)
 
 #print(slices)
 
-#A = []
-for num in text:
-    av = ord(num)
-    #A.append(av)
-    print(av)
+av = [ord(num)for num in text]
+print(av)
+# A = []
+# for num in text:
+#     av = ord(num)
+#     A.append(av)
+    # print(av)
    
 #print(A)
 # input numerical data
@@ -25,16 +29,27 @@ freq_spectrum = np.fft.fft(av)
 freq = np.fft.fftfreq(len(av))
 print(freq)
 
-# get the corresponding frequencies in Hz
+
+# # get the corresponding frequencies in Hz
 sampling_rate = 44100
+duration = 1
 freq_hz = freq * sampling_rate
 plt.plot(freq_hz, abs(freq_spectrum))
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Amplitude')
 plt.show()
 
-time_waveform = np.fft.ifft(freq_spectrum)
-sf.write('test.wav', time_waveform.real, sampling_rate)
+# #concatenate the frequencies to the
+t = np.linspace(0, duration, sampling_rate * duration, False)
+waveform = np.concatenate([np.sin(2 * np.pi * f * t) for f in av])
+
+sf.write('mycode.wav',waveform, sampling_rate)
+
+
+# time_waveform = np.fft.ifft(freq_spectrum)
+ #sf.write('mycode.wav', waveform, sampling_rate)
+
+
 
 
 #for slice in slices:
